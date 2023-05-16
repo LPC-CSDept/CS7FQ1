3c8a52a00ff3ffeecf7de54cdc29c1ce5e2c84a2
Kyu Lee <kwooler@gmail.com>
2023-05-15 23:17:51 -0700

    CS7FQ1

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
+import pandas as pd
+
+
 class Names:
+    numNames = 0
     #################
     # Implement the class Names
-    pass
+    #################
+
+    def __init__(self, n, g, c):
+        self._name = n
+        self._gender = g
+        self._count = c
+        Names.numNames += 1
+
+
+def makeList(filename):
+    df = pd.read_csv(filename, delimiter='\s+')
+    namelist = []
+    #################
+    # Code your program
+    #################
+    return namelist
+
+
+def sortbyCount(nlist):
+    #################
+    # Code your program
+    #################
+
+
+def printList(nlist):
+    print(f'Total number of Names: {Names.numNames}')
+    for v in nlist:
+        print(v)
+
+
+def printName(nlist, key):
+    cnt = 0
+    print(f'################ Names starting with {key} ################')
+    for v in nlist:
+        if v.name.startswith(key):
+            print(v)
+            cnt += 1
+    print(f'################ Total {cnt} names found ################')
+    return cnt
 
 
 def main():
-    # Test code for your class
-    ca = Names('ca2021.txt')
-    print(ca)
-    fl = Names('fl2021.txt')
-    print(fl)
-    print(ca.nlist)
-    print(fl.nlist)
-
-    if ca > fl:
-        print('The name class ca is greater than fl')
-    else:
-        print('The name class fl is greater than or equal to ca')
-
-    ca.printName('C', 'Count', True)
-    ca.printName('D', 'Name', False)
-    fl.printName('A', 'Count', True)
-    fl.printName('E', 'Name', False)
+    nlist = makeList('ca2021.txt')
+    sortbyCount(nlist)
+    printList(nlist)
+    ret = printName(nlist, 'D')
+    ret = printName(nlist, 'R')
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -2,53 +2,40 @@ import random
 import main
 
 
+def test_name():
+    obj1 = main.Names('John', 'Female', 100)
+    obj2 = main.Names('Mary', 'Female', 10)
+
+    assert obj1.name == 'John'
+    assert obj1.count == 100, 'expected 100 (numeric)'
+    assert obj1.__str__ != None
+
+    assert (obj1 > obj2) == True, 'expected True'
+    assert (obj1 < obj2) == False, 'expected False'
+
+
 def test_init():
-    ca = main.Names('ca2021.txt')
+    main.Names.numNames = 0
+    ca = main.makeList('ca2021.txt')
 
-    lenofdict = len(ca._nlist)
-    assert lenofdict == 200, "Invalid value. Expected 200"
-
-    cnt = len([d for d in ca.nlist if d['Gender'] == 'F'])
-    assert cnt == 100, "Invalid value. Expected 100"
-    cnt = len([d for d in ca.nlist if d['Gender'] == 'M'])
-    assert cnt == 100, "Invalid value. Expected 100"
-
-    mydict = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                    'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca._nlist = mydict
-    assert len(ca._nlist) == 3, "Expected 3"
-
-
-def test_str():
-    ca = main.Names('ca2021.txt')
-    print(ca)
+    lenlist = len(ca)
+    print(f'Total number of Names objects in the list: {lenlist}')
+    assert lenlist == 200, "Invalid value. Expected 200"
+    print(f'Class Attribute numNames:  {main.Names.numNames}')
+    assert main.Names.numNames == 200, " 200 "
     assert ca.__str__ != None
 
 
 def test_gt():
-    ca = main.Names('ca2021.txt')
-    fl = main.Names('fl2021.txt')
-    result = ca > fl
-    assert result == True, 'Expected True'
-
-    mydict1 = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                     'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca.nlist = mydict1
-    mydict2 = [{'Name': 'Oliver', 'Count': 1523, 'Gender': 'M'}, {'Name': 'Luna', 'Count': 1458, 'Gender': 'F'}, {
-        'Name': 'Elijah', 'Count': 1498, 'Gender': 'M'}, {'Name': 'Amelia', 'Count': 1363, 'Gender': 'F'}]
-    fl.nlist = mydict2
-    result = ca > fl
-    assert result == False, 'Expected False'
+    ca = main.makeList('ca2021.txt')
+    ca.sort(reverse=True)
+    print('1st element after sorting', ca[0])
+    assert ca[0].name == 'Noah', 'Expected Noah'
 
 
 def test_printName():
-    ca = main.Names('ca2021.txt')
-    ret = ca.printName('C', 'Count', True)
-    assert ret == 10, 'Expected 10'
-    ret = ca.printName('E', 'Gender', False)
-    assert ret == 23, 'Expected 23'
-    mydict1 = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                     'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca.nlist = mydict1
-    ret = ca.printName('A', 'Count', True)
-    assert ret == 3, 'Expected 3'
+    ca = main.makeList('ca2021.txt')
+    ret = main.printName(ca, 'D')
+    assert ret == 8, 'expected 8'
+    ret = main.printName(ca, 'R')
+    assert ret == 5, 'expected 5'
